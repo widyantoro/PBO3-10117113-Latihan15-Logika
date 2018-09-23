fcdf32c66b27e31a7d7a351d2da43e7c66e29a69
Eka Widyantoro <ekawidyantoro17@gmail.com>
2018-09-23 19:00:01 +0700

    first commit

---
+++ src/pbo3/pkg10117113/latihan15/logika/PBO310117113Latihan15Logika.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan15.logika;
+
+/*
+ * 
+ * @author
+ * Nama     : Eka Widyantoro
+ * Kelas    : IF-3 / PBO3
+ * NIM      : 10117113
+ * Deskripsi Program : Program Logika
+ */
+public class PBO310117113Latihan15Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
